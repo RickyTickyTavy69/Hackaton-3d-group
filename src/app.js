b0d905b56746d00c2481773991de134b3d921cac
Artem Bakirov <bakirovartem69@gmail.com>
2022-03-11 22:00:30 +0100

    created menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 import './styles.css'
 
+import {ContextMenu} from '../src/menu.js'
+
+let siteMenu = new ContextMenu('.menu');
+
+siteMenu.changeColor = function() {
+    body.style = 'background: #88B2B2;'
+}
+
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
 
 export class ContextMenu extends Menu {
 
+    constructor(selector) {
+        super(selector);
+
+        this.el = document.querySelector(selector);
+
+        document.body.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            this.open(event.clientY, event.clientX);
+        })
+    }
+
+    open(top, left){
+        this.el.classList.add('open');
+
+        this.el.style = `top: ${top}px; left:${left}px;`
+    }    
+    
+    close(){
+        this.el.classList.remove('open');
+    }
 }
