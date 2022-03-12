173f8be497c0445705bb8a1556f2ee7c6fed803f
Jonibek9800 <Jonibek.mahmudov1998@mail.ru>
2022-03-12 17:58:42 +0300

    Создан модуль смена фона и подсчет кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,27 @@
-import {Module} from '../core/module'
+import {
+    Module
+} from '../core/module'
+import {
+    createElement
+} from '../utils'
+import {
+    time
+} from '../core/constants/setting'
 
 export class ClicksModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+    }
+    trigger() {
+        const second = prompt('Введите время под счета кликов. Число:').trim()
+        if (isNaN(second)) {
+            alert('Input error is not a number')
+        } else {
+            time = second
+            createElement(time)
+        }
+    }
+    toHTML() {
 
+    }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,46 @@
+import * as setting from './core/constants/setting'
+
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+}
+
+
+export function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+export function createElement(time) {
+  const span = document.createElement('span')
+  span.className = 'timer'
+  span.setAttribute('data-time', `${time}`)
+  span.textContent = '00'
+  container.append(span)
+  container.addEventListener('click', () => setting.Click++)
+  container.addEventListener('dblclick', () => setting.dblClick++)
+  setting.inter = setInterval(() => {
+    decreaseTime(span)
+  }, 1000)
+
+}
+
+
+
+function decreaseTime(span) {
+  if (time === 0) {
+    setting.Click -= (setting.dblClick * 2)
+    alert(`Сделано кликов одинарное: ${setting.Click}, двойное ${setting.dblClick}`)
+    clearInterval(setting.inter)
+    span.remove()
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    span.innerHTML = current
+  }
 }
