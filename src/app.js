cd683e9aae566be8f33e1ceda27f2c0bc3075487
Karen Habuzian <karen.gabuzyan@gmail.com>
2022-03-12 21:45:02 +0300

    add hide2

--- src/app.js
+++ src/app.js
@@ -4,13 +4,4 @@ import { ShapeModule } from './modules/shape.module'
 // Создание случайной фигуры случайного цвета
 const randomShape = new ShapeModule('shape', 'Создать случайную фигуру');
 randomShape.toHTML();
-// randomShape.trigger();
-
-const ev = document.querySelector('body');
-ev.addEventListener('click', (event) => {
-    const {target} = event;
-    if (target) {
-        randomShape.trigger();
-    }
-})
-
+randomShape.trigger();
