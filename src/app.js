d3f55c810f6aeb6ec0d607f70fb3aea184d81473
Karen Habuzian <karen.gabuzyan@gmail.com>
2022-03-13 12:15:22 +0300

    fixed audio playing

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import './styles.css'
-
 import { AudioModule } from './modules/audio.module'
 
 // случайный звук
 const randomAudio = new AudioModule('audio', 'Случайный звук');
 randomAudio.toHTML();
-// randomAudio.trigger();
+randomAudio.trigger();
 
 
+///////////  для теста - удалить при слиянии веток 
 window.addEventListener('click', (event) => {
     const { target } = event;
     if (target) {
         randomAudio.trigger();
     }
 })
+//////////////////////////////////////////////
--- src/modules/audio.module.js
+++ src/modules/audio.module.js
@@ -1,28 +1,26 @@
 import {Module} from '../core/module'
 import * as Utils from '../utils'
 
+
 export class AudioModule extends Module {
     constructor(type, text) {
         super(type, text);
     }
 
-    switchTreck(player, randNum) {
-        player.src = `../audio/gameshow_${randNum}.mp3`;
+    switchTreck(player, source, randNum) {
+        source.src = `src/audio/treck_${randNum}.mp3`;
         player.play();
     }
 
     trigger() {
         this.hide();
-        const bodyHTML = document.querySelector('body');
         const audioPlayer = document.createElement('audio');
-        // audioPlayer.setAttribute('controls', '');
-
         const randomNumber = Utils.random(1, 15);
         const sourceHTML = document.createElement('source');
         sourceHTML.setAttribute('type', 'audio/mp3');
-        this.switchTreck(audioPlayer, randomNumber);
+        this.switchTreck(audioPlayer, sourceHTML, randomNumber);
         audioPlayer.append(sourceHTML);
-        bodyHTML.append(audioPlayer);
+        document.body.append(audioPlayer);
     }
 
     hide() {
@@ -35,5 +33,4 @@ export class AudioModule extends Module {
     toHTML() {
         return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`;
     }
-
 }
