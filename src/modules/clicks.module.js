6af3e10ac8a25739f89e5615147e03885de23819
Jonibek9800 <Jonibek.mahmudov1998@mail.ru>
2022-03-13 20:39:40 +0300

    исправлено баги с таймером и с кликамы

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,58 +1,79 @@
 import {
-    Module
+  Module
 } from '../core/module'
 let timeOne = 0
 let dblClick = 0
 let Click = 0
 let inter
 const body = document.querySelector('body')
+
+
 export class ClicksModule extends Module {
-    constructor(type, text) {
-        super(type, text)
-        this.type = type
-        this.text = text
+  constructor(type, text) {
+    super(type, text)
+    this.type = type
+    this.text = text
+  }
+  trigger() {
+    const spanTime = document.querySelector('.click-count')
+    const second = prompt('Введите время под счета кликов. Число:').trim()
+    if (isNaN(second)) {
+      alert('Input error is not a number')
+    } else {
+      timeOne = second
+      document.addEventListener('click', clickOut)
+      document.addEventListener('dblclick', dblClickOut)
+      if (spanTime) {
+        alert('Таймер уже запушен.')
+      } else {
+        createElement(timeOne)
+      }
     }
-    trigger() {
-        const second = prompt('Введите время под счета кликов. Число:').trim()
-        if (isNaN(second)) {
-            alert('Input error is not a number')
-        } else {
-            timeOne = second
-            createElement(timeOne)
-        }
-    }
-    toHTML() {
-        return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
-    }
+  }
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+  }
+}
+/*=======одинарный клик====== */
+function clickOut() {
+  Click++
+}
+/*=======двойной клик====== */
+function dblClickOut() {
+  dblClick++
+}
+/*==========окно таймера============*/
+function createElement(time) {
+  const span = document.createElement('span')
+  span.className = 'click-count'
+  span.setAttribute('data-time', `${time}`)
+  span.textContent = '00'
+  body.append(span)
+
+
+  inter = setInterval(() => {
+    decreaseTime(span)
+  }, 1000)
+
 }
 
-function createElement(time) {
-    const span = document.createElement('span')
-    span.className = 'click-count'
-    span.setAttribute('data-time', `${time}`)
-    span.textContent = '00'
-    body.append(span)
-    body.addEventListener('click', () => Click++)
-    body.addEventListener('dblclick', () => dblClick++)
-    inter = setInterval(() => {
-      decreaseTime(span)
-    }, 1000)
-  
-  }
-  
-  
-  
-  function decreaseTime(span) {
-    if (timeOne === 0) {
-      Click -= (dblClick * 2)
-      alert(`Сделано кликов одинарное: ${Click}, двойное ${dblClick}`)
-      clearInterval(inter)
-      span.remove()
-    } else {
-      let current = --timeOne;
-      if (current < 10) {
-        current = `0${current}`;
-      }
-      span.innerHTML = current
+
+/*==========Таймер для клика============ */
+function decreaseTime(span) {
+  if (timeOne === 0) {
+    Click -= (dblClick * 2)
+    document.removeEventListener('dblclick', dblClickOut)
+    document.removeEventListener('click', clickOut)
+    alert(`Сделано кликов одинарное: ${Click}, двойное ${dblClick}`)
+    dblClick = 0
+    Click = 0
+    clearInterval(inter)
+    span.remove()
+  } else {
+    let current = --timeOne;
+    if (current < 10) {
+      current = `0${current}`;
     }
+    span.innerHTML = current
   }
+}
